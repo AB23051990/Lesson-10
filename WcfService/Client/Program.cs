cbd3aa0a062b970fd7579bc78938318ade0ad2d2
Your Name <ab23051990@yandex.ru>
2022-12-07 23:32:57 +0300

    HW2

---
+++ WcfService/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Client.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+            PumpnServiceClient client = new PumpnServiceClient(instanceContext);
+
+            client.UpdateAndCompileScript(@"C:\Users\user\Desktop\geekbrains\Lesson-10\WcfService\WcfService\Scripts\Sample.script");
+            client.RunScript();
+
+            Console.WriteLine("Please, Enter to exit ...");
+            Console.ReadKey(true);
+            client.Close();
+        }
+    }
+}
